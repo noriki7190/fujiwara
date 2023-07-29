a63c14b30b7509a3aaebd5e97399a543a78f3c48
noriki7190 <noriki7190@gmail.com>
2023-07-29 22:52:24 +0900

    7/29 作業分

--- dev/src/assets/js/script.js
+++ dev/src/assets/js/script.js
@@ -1,1 +1,16 @@
-console.log('test');
+jQuery(function ($) {
+  // アコーディオンメニュー
+  $('.js-accordion').on('click', function () {
+    console.log('click')
+    $(this).toggleClass('is-open');
+    $(this).next().slideToggle();
+  });
+
+  // ドロワー
+  var drawer = $("#js-drawer-icon");
+  drawer.on("click", function () {
+    drawer.toggleClass("is-open");
+    $("#js-drawer").toggleClass("is-open");
+    // $("#js-drawer").show();
+  });
+});
