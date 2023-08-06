75449ef5acb30a40ace6f431d3eb51ce474e07be
noriki7190 <noriki7190@gmail.com>
2023-08-06 11:46:11 +0900

    トップページパララックス完成

--- dev/src/assets/js/script.js
+++ dev/src/assets/js/script.js
@@ -1,7 +1,7 @@
 jQuery(function ($) {
   // アコーディオンメニュー
   $('.js-accordion').on('click', function () {
-    console.log('click')
+    // console.log('click')
     $(this).toggleClass('is-open');
     $(this).next().slideToggle();
   });
@@ -12,5 +12,22 @@ jQuery(function ($) {
     drawer.toggleClass("is-open");
     $("#js-drawer").toggleClass("is-open");
     // $("#js-drawer").show();
+  });
+  
+  // scroll
+  $(window).on('load scroll resize', function() {
+    const scrollTop = $(window).scrollTop();
+    // console.log(scrollTop);
+    const showImgPosition = $('#js-parallax').offset().top - 1000;
+    // console.log(showImgPosition);
+    const showClass = 'is-show';
+
+    if (scrollTop >= showImgPosition) {
+      $('#js-parallax').addClass(showClass);
+      // console.log('show');
+    } else {
+      $('#js-parallax').removeClass(showClass);
+      // console.log('no-show');
+    }
   });
 });
